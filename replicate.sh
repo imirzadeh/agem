356bde4b7c2a3144d7da0d7693ae328bbc2d9e4e
Iman Mirzadeh <seyediman.mirzadeh@wsu.edu>
2020-04-14 18:50:08 +0000

    Add A-GEM specific run script

---
+++ replicate.sh
@@ -0,0 +1,9 @@
+ARCH='FC-S'
+NUM_RUNS=1
+BATCH_SIZE=10
+OPTIM='SGD'
+lr=0.03
+lam=0.1
+RESULTS_DIR='results/mnist'
+python3 ./fc_permute_mnist.py --train-iters 25000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'A-GEM' --synap-stgth $lam --log-dir $RESULTS_DIR
+
