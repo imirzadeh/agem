386faeebd204d94a55d72817263e9deec8da9e9f
Iman Mirzadeh <iman.kinder@gmail.com>
2020-04-14 19:16:55 -0700

    Fix package init typo

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .data_utils import construct_permute_mnist, construct_split_mnist, construct_split_cifar, construct_split_cub, construct_split_imagenet 
+from .data_utils import construct_permute_mnist, construct_split_mnist, construct_split_cifar, construct_rotate_mnist
 from .data_utils import image_scaling, random_crop_and_pad_image, random_horizontal_flip 
 from .utils import clone_variable_list, create_fc_layer, create_conv_layer, sample_from_dataset, update_episodic_memory, update_episodic_memory_with_less_data, concatenate_datasets 
 from .utils import samples_for_each_class, sample_from_dataset_icarl, get_sample_weights, compute_fgt, load_task_specific_data, load_task_specific_data_in_proportion
