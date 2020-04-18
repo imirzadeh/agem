adf0423f7624cefd214a7d2540d43e5f9a8100ab
Iman Mirzadeh <iman.kinder@gmail.com>
2020-04-18 15:49:41 -0700

    reduce batch size

--- replicate.sh
+++ replicate.sh
@@ -1,7 +1,7 @@
 ARCH='RESNET-S'
 NUM_RUNS=1
-BATCH_SIZE=64
-EPS_MEM_BATCH_SIZE=64
+BATCH_SIZE=16
+EPS_MEM_BATCH_SIZE=16
 
 # METHOD='ER-Ring'
 OPTIM='SGD'
@@ -14,4 +14,20 @@ if [ ! -d $LOG_DIR ]; then
 fi
 
 # python3 hal_mnist.py --train-single-epoch --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --learning-rate 0.1 --imp-method VAN --synap-stgth 0.0 --log-dir $LOG_DIR --mem-size 1 --examples-per-task 50000 --eps-mem-batch $EPS_MEM_BATCH_SIZE --anchor-eta 0.1
-python3 ./conv_split_cifar.py --train-single-epoch --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method $imp_method --synap-stgth $lam --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 1234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method $imp_method --synap-stgth 0.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 7295 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method $imp_method --synap-stgth 0.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 5234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method $imp_method --synap-stgth 0.0 --log-dir $LOG_DIR
+
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 1234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method EWC --synap-stgth 10.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 7295 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method EWC --synap-stgth 10.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 5234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method EWC --synap-stgth 10.0 --log-dir $LOG_DIR
+
+
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 1234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method ER --synap-stgth 0.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 7295 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method ER --synap-stgth 0.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 5234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method ER --synap-stgth 0.0 --log-dir $LOG_DIR
+
+
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 1234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method VAN --synap-stgth 0.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 7295 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method VAN --synap-stgth 0.0 --log-dir $LOG_DIR
+python3 ./conv_split_cifar.py --train-single-epoch --mem-size 1 --arch $ARCH --random-seed 5234 --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method VAN --synap-stgth 0.0 --log-dir $LOG_DIR
