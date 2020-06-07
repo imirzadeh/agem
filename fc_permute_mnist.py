f3db8ae9022555993138a92a11e6f92b3a0e31b5
Iman Mirzadeh <iman.kinder@gmail.com>
2020-06-06 20:04:01 -0700

    A desperate attempt to print tensors in tensorflow :|

--- fc_permute_mnist.py
+++ fc_permute_mnist.py
@@ -218,7 +218,7 @@ def train_task_sequence(model, sess, args):
             print('Received {} images, {} labels at task {}'.format(train_x.shape[0], train_y.shape[0], task))
             print("get global step")
             tf.print(model.global_step, output_stream=sys.stderr)
-            tf.print(model.learning, output_stream=sys.stderr)
+            tf.print(model.learning_rate, output_stream=sys.stderr)
             print('----'*5)
             # Array to store accuracies when training for task T
             ftask = []
