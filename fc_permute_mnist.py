811fc03b01e0508d268588825616530a868ad76d
Iman Mirzadeh <iman.kinder@gmail.com>
2020-06-06 19:27:50 -0700

    Add log to global step

--- fc_permute_mnist.py
+++ fc_permute_mnist.py
@@ -217,7 +217,7 @@ def train_task_sequence(model, sess, args):
             
             print('Received {} images, {} labels at task {}'.format(train_x.shape[0], train_y.shape[0], task))
             print("get global step")
-            tf.print(model.global_step)
+            tf.print(model.global_step, output_stream=sys.stderr)
             # Array to store accuracies when training for task T
             ftask = []
             
