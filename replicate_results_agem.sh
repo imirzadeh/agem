0112aa239e32a108bef228a6424924416587146f
Iman Mirzadeh <iman.kinder@gmail.com>
2020-06-06 22:18:06 -0700

    Add more trials

--- replicate_results_agem.sh
+++ replicate_results_agem.sh
@@ -12,8 +12,18 @@ lr=0.1
 ARCH='FC-S'
 OPTIM='SGD'
 
-python3 ./fc_permute_mnist.py --random-seed 1215 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'A-GEM' --log-dir $LOG_DIR
-python3 ./fc_permute_mnist.py --random-seed 1215 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'ER' --log-dir $LOG_DIR
+python3 ./fc_permute_mnist.py --random-seed 1417 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'A-GEM' --log-dir $LOG_DIR
+python3 ./fc_permute_mnist.py --random-seed 1417 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'ER' --log-dir $LOG_DIR
+
+python3 ./fc_permute_mnist.py --random-seed 1315 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'A-GEM' --log-dir $LOG_DIR
+python3 ./fc_permute_mnist.py --random-seed 1315 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'ER' --log-dir $LOG_DIR
+
+
+python3 ./fc_permute_mnist.py --random-seed 2912 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'A-GEM' --log-dir $LOG_DIR
+python3 ./fc_permute_mnist.py --random-seed 2912 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'ER' --log-dir $LOG_DIR
+
+python3 ./fc_permute_mnist.py --random-seed 3114 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'ER' --log-dir $LOG_DIR
+python3 ./fc_permute_mnist.py --random-seed 3114 --decay 0.75 --examples-per-task 50000 --arch $ARCH --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --optim $OPTIM --learning-rate $lr --imp-method 'ER' --log-dir $LOG_DIR
 
 # python conv_split_cifar.py --train-single-epoch --cross-validate-mode --num-runs $NUM_RUNS --batch-size $BATCH_SIZE --learning-rate 0.1 --imp-method $IMP_METHOD --synap-stgth 0 --log-dir $LOG_DIR --mem-size $MEM_SIZE --eps-mem-batch $EPS_MEM_BATCH_SIZE
 
